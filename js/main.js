bd210e774562cb9cb897e066d6131c88730fc257
Ahmed Karam <ahmedkaramali710@outlook.com>
2024-09-18 18:08:00 +0300

    add navbar responsive

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
-let togle = document.getElementsByClassName(".dropdown_menu");
-let;
+let mneuLinks = document.getElementById("mneuLinks");
+
+let toggleBtn = document.getElementById("toggole");
+
+mneuLinks.style.maxHeight = "0px";
+function showLinks() {
+  if (mneuLinks.style.maxHeight === "0px") {
+    mneuLinks.style.maxHeight = "420px";
+  } else {
+    mneuLinks.style.maxHeight = "0px";
+  }
+}
+
+toggleBtn.addEventListener("click", showLinks);
+
+// let submit = document.getElementById("submit");
+// submit.addEventListener("mouseover", () => {
+//   submit.style.transition = "all 0.3s";
+//   console.log("Error");
+// });
